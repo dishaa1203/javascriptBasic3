c75bcf89027565cdf598a8681760c6d1f03a782e
dishaa1203 <disha12032005@gmail.com>
2026-01-09 21:57:07 +0530

    Add files via upload
    Implemented while loop for numeric and array-based iteration
    Demonstrated do-while loop execution order (run first, check later)
    Practiced loop control using counters and conditions

---
+++ whileDoWhileLoop.js
@@ -0,0 +1,26 @@
+//while  loop
+
+let i = 0
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i=i+2
+}
+
+let myArray =["spiderman","ironman","captainamerica","thor"]
+
+let arr = 0
+while (arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1 
+}
+
+//do while loop
+//first work is done and then the condition is checked
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++  
+} while (score <= 10);
+
